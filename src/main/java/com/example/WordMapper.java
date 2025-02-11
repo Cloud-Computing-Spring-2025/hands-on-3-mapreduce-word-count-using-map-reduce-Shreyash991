d522fbddaf717e0a19ac2c6156e647aba05b1d61
Shreyash Thakre <sthakre@uncc.edu>
2025-02-11 18:25:51 +0000

    Successfully implemented Mapreduce

--- src/main/java/com/example/WordMapper.java
+++ src/main/java/com/example/WordMapper.java
@@ -1,5 +1,21 @@
 package com.example;
 
-public class WordMapper{
-    
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+
+public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private final static IntWritable count = new IntWritable(1);
+    private Text word = new Text();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] wordList = value.toString().split(" ");
+        for(String currWord : wordList) {
+            word.set(currWord);
+            context.write(word, count);
+        }
+    }
 }
--- src/main/java/com/example/controller/Controller.java
+++ src/main/java/com/example/controller/Controller.java
@@ -1,9 +1,33 @@
 package com.example.controller;
+import com.example.WordMapper;
+import com.example.WordReducer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import java.io.IOException;
 
 
 public class Controller{
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+          Configuration config=new Configuration();
+          Job job=Job.getInstance(config,"Word count");
+
+          job.setJarByClass(Controller.class);
+          job.setMapperClass(WordMapper.class);
+          job.setReducerClass(WordReducer.class);
+
+          job.setOutputKeyClass(Text.class);
+          job.setOutputValueClass(IntWritable.class);
+
+          FileInputFormat.addInputPath(job,new Path(args[0]));
+          FileOutputFormat.setOutputPath(job,new Path(args[1]));
+
+          System.exit(job.waitForCompletion(true)?0:1);
+  
     }
 }
